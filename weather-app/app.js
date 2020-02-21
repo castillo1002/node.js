1c9de8a8f7cfa8b03d8e0f151be0537e565e2941
castillo1002 <castillo@mitcon.org>
2020-02-21 08:09:23 -0500

    Destructuring and Object Shorthand

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,18 +2,18 @@ const forecast = require('./utils/forecast')
 const geocode = require('./utils/geocode')
 
 
-geocode(process.argv[2], (error, data) => {
+geocode(process.argv[2], (error, { longitude, latitude, location }) => {
     if (!process.argv[2]) {
         return console.log('Please provide a location')
     }
     if (error) {
         return console.log('Error: ', error)
     }
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
+    forecast(latitude, longitude, (error, forecastData) => {
         if (error) {
             return console.log('Error: ', error)
         }
-        console.log(data.location)
+        console.log(location)
         console.log(forecastData)
     })
 })
--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -2,13 +2,13 @@ const request = require('request')
 
 const forecast = (lat, long, callback) => {
     url = `https://api.darksky.net/forecast/772f03f5ba7c5acb93e1997e37edc66a/${lat},${long}`
-    request({ url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Cannot reach weather service', undefined)
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location, try again', undefined)
         } else {
-            callback(undefined, `${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} degrees, with a %${response.body.currently.precipProbability} chance of rain`)
+            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees, with a %${body.currently.precipProbability} chance of rain`)
         }
     })
 }
