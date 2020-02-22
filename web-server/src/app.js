10a3fbe5a0f809fdc9e9541ebcf8a3a5d011993d
Castillin <Castillin@MacBook-Pro.local>
2020-02-22 16:45:27 -0500

    Create 404 Templates

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -48,6 +48,22 @@ app.get('/weather', (req, res) => {
     })
 })
 
+app.get('/help/*', (req, res) => {
+    res.render('/404', {
+        title: '404 Page',
+        name: 'Arturo Castillo',
+        errorMsg: 'Help article not found'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('/404', {
+        title: '404 Page',
+        name: 'Arturo Castillo',
+        errorMsg: 'Page not found'
+    })
+})
+
 app.listen(3000, () => {
     console.log('The server is up on port 3000')
 })
