37873cbf82277ecbc82caf06dc6e78d6262ee8e4
Castillin <Castillin@MacBook-Pro.local>
2020-02-22 16:02:06 -0500

    Partial Creating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,32 +1,23 @@
-// Express only exposes a single function
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
-// In order to use html files for sending using express, we have to indicate the path of the file where our html code resides.  We can use two handy methods provided by nodejs for finding the path:
-
-console.log(__dirname)
-console.log(__filename)
-
-// __dirname will display the full path of the dir where our file lives
-// __filename will display the full path of where the file itself lives
-
-// There is a core module called path that we can use to manipulate paths of dirs and files.  We use the join method to provide the different pieces of the path, and join will join them together in a string.  In the 1st arg, we use the __dirname built in method, and in the 2nd arg, we can traverse the folder structure to provide the path to a file or folder in a different directory:
-
-console.log(path.join(__dirname, '../public/index.html'))
+// Define paths for Express config:
 const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
+// Setup handlebars engine and views location
+app.set('views', viewsPath)
 app.set('view engine', 'hbs')
+hbs.registerPartials(partialsPath)
 
+// Setup static dir to serve
+app.use(express.static(publicDirectoryPath))
 
-
-// Example domain: app.com
-// Routes:
-// app.com/help
-// app.com/about
-
-// app.get denotes what we should do, when someone makes a request to a specific url.  Takes 2 arguments: 1. Route 2. Callback Function  The callback function also has two important arguments, the first is info about the request that was made (req), and the second is the response (res) which allows us to customize what we are going to send to the requestor by using its methods:
+// The res.render method allows us to render one of our view files and also specify what data our view should be able to access by using an object in the 2nd argument
 
 app.get('', (req, res) => {
     res.render('index', {
@@ -35,10 +26,20 @@ app.get('', (req, res) => {
     })
 })
 
-// Method used to customize our server which in this case lets us serve up the static assets (html, images, videos, etc) in a specific directory.  With this we don't need to create separate routes for assets that live in the public folder:
-app.use(express.static(publicDirectoryPath))
+app.get('/help', (req, res) => {
+    res.render('help', {
+        message: 'This is my help message',
+        title: 'Help',
+        name: 'Arturo Castillo'
+    })
+})
 
-// When we send an object as a response, express will stringify our JSON object for us.  This can be an obj, or an array.
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About page',
+        name: 'Arturo Castillo'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
@@ -47,13 +48,6 @@ app.get('/weather', (req, res) => {
     })
 })
 
-
-// To start server:  (Port 3000 is commonly used for development).  When it is running, your terminal will hang since the process is still listening.
-
 app.listen(3000, () => {
     console.log('The server is up on port 3000')
 })
-
-// After the server has started, it will run on your local machine and will be accessible on localhost:3000
-
-// If you try to access a route that is not in the application, you will get an error, which should be handled with something like a 404 page
