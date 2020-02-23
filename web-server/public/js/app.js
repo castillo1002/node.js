4b55f35146ca8afd8fceaf34849b968558e4ab56
Castillin <Castillin@MacBook-Pro.local>
2020-02-23 16:14:11 -0500

    Displaying Weather Data in HTML and Styling

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -2,18 +2,27 @@ console.log('Clientside Javascript is loaded!!!')
 
 
 
-const weatherForm = document.querySelector('form')
+const weatherForm = document.querySelector('button')
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
 
 weatherForm.addEventListener('click', (e) => {
     e.preventDefault()
     const location = search.value
 
+    messageOne.textContent = 'Loading data...'
+    messageTwo.textContent = ''
+
+
     fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
         response.json().then((data) => {
             if (data.error) {
-                console.log(data.error)
+                messageOne.textContent = data.error
             } else {
+                messageOne.textContent = ''
+                messageTwo.textContent = data.location + data.forecast
                 console.log(data.location)
                 console.log(data.forecast)
             }
