8c05100e12d5a31d535729c3af24e7f56d2c4703
Castillin <Castillin@MacBook-Pro.local>
2020-02-23 00:05:01 -0500

    change

--- web-server/utils/geocode.js
+++ web-server/utils/geocode.js
@@ -4,12 +4,12 @@ const geocode = (address, callback) => {
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1IjoiY2FzdGlsbG8xMDAyIiwiYSI6ImNrNnRoODRjYzA0b3Uza2xueHAxbGx1M2wifQ.0G4k8P8FIa5Ub7NqTlDgIQ`
 
     request({ url, json: true }, (error, { body }) => {
+        console.log(body)
         if (error) {
             callback('Unable to connect to location services', undefined)
         } else if (body.features.length === 0) {
             callback('Unable to find location. Try again', undefined)
         } else {
-            console.log(body)
             callback(undefined, {
                 latitude: body.features[0].center[1],
                 longitude: body.features[0].center[0],
